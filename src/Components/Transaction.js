6b327c911df4b9d27ee0c2e051912a3ce605e401
SAURABH YADAV <saurabhshadow30@gmail.com>
2021-06-15 18:12:08 +0530

    Minor changes

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -46,38 +46,33 @@ class Transaction extends React.Component{
     render() {
 
 
-        let page=
-            <Container style={{paddingTop:40}}>
-
-                <Grid container justify="center" >
-                    <Grid item >
-                        <Paper style={{height:300,width:500,paddingTop:'30px',background:'#f8f8f8'}}  >
-                            <text style={{fontSize:"30px",margin:'80px',fontFamily:'Monospace',color:'#090809'}}>Money Transaction</text>
-                            <br/>
-                            <RadioGroup style={{paddingLeft:'100px', paddingTop:'10px'}} aria-label="mode"  value={this.state.mode}
-                                        onChange={(e) => this.handleChange({ mode: e.target.value })}>
-                                <FormControlLabel value="Withdraw" control={<Radio />} label="Withdraw" />
-                                <FormControlLabel value="Deposit" control={<Radio />} label="Deposit" />
-
-                            </RadioGroup>
-                            <input  style={{marginTop:"20px",marginLeft:'90px',paddingLeft:'20px',width: "60%"}} type="text" value={this.state.balance}
-                                    onChange={(e) => this.handleChange({ balance: e.target.value })} />
-                            <br/>
-                            <Button  style={{marginTop:"20px",marginLeft:'160px',width:"30%"}} variant="contained" onClick={this.handleClick}>Submit</Button>
-                        </Paper>
-
-                    </Grid>
-
-
-
-                </Grid>
-
-            </Container>;
-
         return(
             <div>
                 <ToastContainer/>
-                {page}
+                <Container style={{paddingTop:40}}>
+
+                    <Grid container justify="center" >
+                        <Grid item >
+                            <Paper style={{height:300,width:500,paddingTop:'30px',background:'#f8f8f8'}}  >
+                                <text style={{fontSize:"30px",margin:'80px',fontFamily:'Monospace',color:'#090809'}}>Money Transaction</text>
+                                <br/>
+                                <RadioGroup style={{paddingLeft:'100px', paddingTop:'10px'}} aria-label="mode"  value={this.state.mode}
+                                            onChange={(e) => this.handleChange({ mode: e.target.value })}>
+                                    <FormControlLabel value="Withdraw" control={<Radio />} label="Withdraw" />
+                                    <FormControlLabel value="Deposit" control={<Radio />} label="Deposit" />
+
+                                </RadioGroup>
+                                <input  style={{marginTop:"20px",marginLeft:'90px',paddingLeft:'20px',width: "60%"}} type="text" value={this.state.balance}
+                                        onChange={(e) => this.handleChange({ balance: e.target.value })} />
+                                <br/>
+                                <Button  style={{marginTop:"20px",marginLeft:'160px',width:"30%"}} variant="contained" onClick={this.handleClick}>Submit</Button>
+                            </Paper>
+
+                        </Grid>
+
+                    </Grid>
+
+                </Container>
             </div>
 
 
